18bc1d5114c6139ce006cab544d6aa700589ac88
Emilien Hainaux <emilienhainaux@yahoo.fr>
2024-09-18 15:44:19 +0200

    fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
+
 import ConvertView from "../views/ConvertView.vue";
 
-const routes = [
-  { path: "/", name: "home", component: HomeView },
-  { path: "/about", name: "about", component: AboutView },
-  { path: "/convert", name: "convert", component: ConvertView },
-];
+const routes = [{ path: "/", name: "convert", component: ConvertView }];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
